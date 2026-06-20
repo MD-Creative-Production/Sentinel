b0c4a9e07188cc882611b27199fabd033168eb88
AbdulmujibOladayo <aderonkeadejare54@gmail.com>
2026-06-20 18:53:45 +0100

    feat: add barrel export for draining detection module

---
+++ src/modules/detection/draining/index.ts
@@ -0,0 +1,8 @@
+export { DrainingService } from './draining.service';
+export { DrainingModule } from './draining.module';
+export type {
+  BalanceSnapshot,
+  DrainingEvent,
+  DrainingAlert,
+  DrainingDetectorOptions,
+} from './interfaces/draining.interface';
